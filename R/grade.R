837b25aa64edd026f74725ff1a52cd04dbc43a4e
Rino L. Heim <51607254+migilicious@users.noreply.github.com>
2020-03-19 10:00:34 +0100

    Add files via upload

--- R/grade.R
+++ R/grade.R
@@ -1,20 +1,17 @@
-#' @title Grades Creators
+#' @title Grades Creator
 #'
 #' @description This is a function named 'grade' which creates a discontinuous linear scale of grades for the Swiss school grading system. It allows to distinguish the slope of your grading scale for passes and fails.
 #'
-#' @param total The total points
-#' @param pass The min points to pass the exam
-#' @param six The min points to score a six
-#' @param cfactor The correction factor allowing for a discontinuous linear grade scale
-#' @param rnd Rounds the grades
-#' @return The grade
+#' @param total The total points scored.
+#' @param pass The minimum number of points to pass the exam.
+#' @param maxp The maximum number of points possible.
+#' @param cfactor The correction factor allows for a steeper slope of the grading scale for passing grades. The smaller the \code{cfator}, the steeper the slope (default=1).
+#' @param rnd Rounds the grades.
+#' @return Rounds the grades to whatever value you specify (default =0.5).
 #' @export
-grade <- function (total, pass, six, cfactor=1, rnd=0.5) {
-  if(any(is.na(total))) stop("Please drop your NAs")
-  if(pass>=six) warning("Your scale is bullshit")
-    if (total>=pass) {
-    round((((total-pass)*2.25)/((six-pass)*cfactor)+3.75)/rnd)*rnd
-  } else {
-    round(((total*2.75)/(pass)+1)/rnd)*rnd
-  }
+grade <- function (total, pass, maxp, cfactor=1, rnd=0.5) {
+  if(pass>=maxp) stop("Your scale is bullshit")
+  f=round(((total*2.75)/(pass)+1)/rnd)*rnd
+  p=round((((total-pass)*2.25)/((maxp-pass)*cfactor)+3.75)/rnd)*rnd
+  ifelse(total>=pass,f,p)
 }
---
+++ R/gscale.R
@@ -0,0 +1,17 @@
+#' @title Grading Scale
+#'
+#' @description This is a function named 'gscale' which creates the scale of grades for the Swiss school grading system.
+#'
+#' @param pass The minimum number of points to pass the exam.
+#' @param maxp The maximum number of points possible.
+#' @param cfactor The correction factor allows for a steeper slope of the grading scale for passing grades. The smaller the \code{cfator}, the steeper the slope (default=1).
+#' @return The grading scale.
+#' @export
+gscale <- function (pass, maxp, cfactor=1, gr=seq(0.75,6,0.5)) {
+  f=(gr-1)*(pass)/2.75
+  p=(gr-3.75)*((maxp-pass)*cfactor)/2.25+pass
+  points=ifelse(gr>4,p,f)
+  points=ifelse(points<0,0,points)
+  grade=gr+0.25
+  print(cbind(grade,points))
+}
